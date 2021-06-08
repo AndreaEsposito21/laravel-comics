c9bb9e41d191711415bf0e8dca7176d9561b653d
Andrea Esposito <andexpox@gmail.com>
2021-06-08 17:59:33 +0200

    header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,69 @@
 <header>
-    <h2>header</h2>
+    
+    <div class="high-row">
+        <div class="text">
+            <span>text 1</span>
+
+            <span>text 2</span>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <nav>
+            <div class="logo-container">
+                <img src="{{ asset('img/dc-logo.png') }}" alt="Dc logo">
+            </div>
+
+            <div class="navbar">
+                <ul class="menu">
+                    <li>
+                        <a href="#">characters</a>
+                    </li>
+
+                    <li>
+                        <a class="active" href="#">comics</a>
+                    </li>
+
+                    <li>
+                        <a href="#">movies</a>
+                    </li>
+
+                    <li>
+                        <a href="#">tv</a>
+                    </li>
+
+                    <li>
+                        <a href="#">games</a>
+                    </li>
+
+                    <li>
+                        <a href="#">collectibles</a>
+                    </li>
+
+                    <li>
+                        <a href="#">video</a>
+                    </li>
+
+                    <li>
+                        <a href="#">fans</a>
+                    </li>
+
+                    <li>
+                        <a href="#">news</a>
+                    </li>
+
+                    <li>
+                        <a href="#">shop</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="search-bar">
+                Search
+            </div>
+        </nav>
+    </div>
+
+    <div class="banner"></div>
 </header>
